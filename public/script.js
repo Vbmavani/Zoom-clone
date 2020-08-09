f2a92687a349d357e51bb0bb74a99e4e707e964e
vbmavani <vbmavani003@gmail.com>
2020-08-09 12:19:20 +0530

    changed Port of Peer server

--- public/script.js
+++ public/script.js
@@ -1,10 +1,12 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer(undefined, {
-  secure: true,
-  host: 'yaariyan.herokuapp.com',
-  port: 443,
-})
+const peer = new Peer({ host: 'yaariyan.herokuapp.com', secure: true, port: 443 })
+
+// const myPeer = new Peer(undefined, {
+//   secure: true,
+//   host: 'yaariyan.herokuapp.com',
+//   port: 443,
+// })
 const myVideo = document.createElement('video')
 myVideo.muted = true
 const peers = {}
